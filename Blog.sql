31069bb0a18626fb8fb33a2ee1c762de8b6e4f89
yohanafaria <145880332+yohanafaria@users.noreply.github.com>
2024-07-22 11:57:29 -0300

    Add files via upload

---
+++ Blog.sql
@@ -0,0 +1,10 @@
+create database db_blog;
+use db_blog;
+
+INSERT INTO tb_postagens (data, texto, titulo) 
+VALUES (current_timestamp(), 'Texto da postagem 01', 'Postagem 01');
+INSERT INTO tb_postagens (data, texto, titulo) 
+VALUES (current_timestamp(), 'Texto da postagem 02', 'Postagem 02');
+
+SELECT * FROM tb_postagens;
+
